45acacd105849a2afd59a79994998e852330a576
ezebran <ezebran@gmail.com>
2021-10-29 19:01:42 -0300

    ponencias end

---
+++ js/ponencias.js
@@ -0,0 +1,11 @@
+$("#mesa_dia2").hide();
+
+$("#btn_dia1").click(function () {
+    $("#mesa_dia2").hide();
+    $("#mesa_dia1").show();
+});
+
+$("#btn_dia2").click(function () {
+    $("#mesa_dia1").hide();
+    $("#mesa_dia2").show();
+});
